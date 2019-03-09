467ed8da838652f152bf35387f4c42cf3abd8bbf
mayartmal <mayartmal@gmail.com>
2019-03-09 18:41:14 +0300

    work with ide was tested

---
+++ exampleSbtIDEAProject/src/main/scala/Example.scala
@@ -0,0 +1,7 @@
+object Example extends App {
+  val message = "Hi"
+  val postfix = "!"
+  val result = message + postfix
+  println(result)
+
+}
---
+++ exampleSbtIDEAProject/testWorkSheet.sc
@@ -0,0 +1,3 @@
+val smthForOutput = "This is my result for worksheet"
+print(smthForOutput)
+println("Hello Friend")
